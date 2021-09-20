0ea667215d29a2dbbaef8bbf24228961f65bb73b
veronica <veronicaweiss77@gmail.com>
2021-09-20 15:37:59 +0200

    burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,48 @@
 import classes from "./Header.module.css"
 import { NavLink } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export const Header = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [isBurger, setIsBurger] = useState(false)
+  const [navStyle, setNavStyle] = useState({
+    right: "-50vw",
+  })
+
+  window.addEventListener("resize", () => {
+    setScreenWidth(window.innerWidth)
+  })
+
+  useEffect(() => {
+    if (screenWidth <= 590) {
+      setIsBurger(true)
+    } else {
+      setIsBurger(false)
+    }
+  }, [screenWidth])
   return (
     <div className={classes.header}>
       <h1 className={classes.title}>ExchangeCurrencyEasily</h1>
-      <nav className={classes.navigation}>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/exchange">Exchange</NavLink>
-      </nav>
+      {!isBurger ? (
+        <nav className={classes.navigation}>
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/exchange">Exchange</NavLink>
+        </nav>
+      ) : (
+        <>
+          <div onClick={() => setNavStyle({ right: "0" })}>
+            <div className={classes.burger}></div>
+          </div>
+          <nav className={classes.burgerNavigation} style={navStyle}>
+            <div
+              className={classes.close}
+              onClick={() => setNavStyle({ right: "-50vw" })}
+            ></div>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/exchange">Exchange</NavLink>
+          </nav>
+        </>
+      )}
     </div>
   )
 }
